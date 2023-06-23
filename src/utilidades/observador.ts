261e69da5fa99d924d7a59d91a691e48458f9d28
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-06-23 14:47:54 -0500

    🔨 Pasar a typscript

--- src/utilidades/observador.ts
+++ src/utilidades/observador.ts
@@ -1,13 +1,13 @@
 /**
- * @param {HTMLElement | Element} elemento Elemento que se quiere observar.
- * @param {function} accion Función que se ejecuta cuando entra en intersección.
- * @param {Boolean} desconectarInmediatamente Si la acción se debe ejecutar 1 sola vez.
- * @param {Object} opciones Opciones del "Intersection Observer API"
+ * @param elemento Elemento que se quiere observar.
+ * @param accion Función que se ejecuta cuando entra en intersección.
+ * @param desconectarInmediatamente Si la acción se debe ejecutar 1 sola vez.
+ * @param opciones Opciones del "Intersection Observer API"
  * @returns instancia del observador
  */
 export const duranteInterseccion = (
-  elemento,
-  accion,
+  elemento: HTMLElement,
+  accion: (InterdectionObserverEntry) => void,
   desconectarInmediatamente = true,
   opciones = { threshold: 0 }
 ) => {
