c25ff6637805c7ce17fdfe66973e6e34d13928c6
Juliansintilde <jcgc777@gmail.com>
2023-07-05 18:18:57 -0500

    💄 Tipografías y colores

--- src/ambages/aparicionRelato.ts
+++ src/ambages/aparicionRelato.ts
@@ -1,31 +1,56 @@
+import { hexARGB } from '@enflujo/alquimia';
 import { duranteInterseccion } from '../utilidades/observador';
+
+export type ColorGradiante = {
+  porcentaje: number;
+  color: { r: number; g: number; b: number };
+};
 
 let enVista = false;
 let reloj: NodeJS.Timer | null;
 let angulo = 180;
-let prendido = false;
 
-const colores = [
-  [
-    { porcentaje: 0, color: { r: 50, g: 29, b: 47 } },
-    { porcentaje: 100, color: { r: 255, g: 0, b: 0 } },
-  ],
-  [
-    { porcentaje: 0, color: { r: 61, g: 46, b: 79 } },
-    { porcentaje: 100, color: { r: 180, g: 0, b: 0 } },
-  ],
-  [
-    { porcentaje: 0, color: { r: 76, g: 95, b: 122 } },
-    { porcentaje: 100, color: { r: 0, g: 0, b: 0 } },
-  ],
+const colorDesde = [
+  { porcentaje: 0, color: { r: 50, g: 29, b: 47 } },
+  { porcentaje: 0, color: { r: 61, g: 46, b: 79 } },
+  { porcentaje: 0, color: { r: 76, g: 95, b: 122 } },
 ];
 
+const colorHacia = [
+  { porcentaje: 100, color: { r: 255, g: 0, b: 0 } },
+  { porcentaje: 100, color: { r: 180, g: 0, b: 0 } },
+  { porcentaje: 100, color: { r: 0, g: 0, b: 0 } },
+];
+const hacia1 = 'rgb(255, 0, 0)';
+const hacia2 = 'rgb(180, 0, 0)';
+const hacia3 = 'rgb(0, 0, 0)';
+/**
+ * A los elementos con clase `relato`, hace aparecer todos los `<p>` que tenga internamente. También cambia el color del fondo animando el gradiente.
+ * En el HTML se pueden asignar los siguientes parámetros:
+ * - color1
+ * - color2
+ * - color3
+ * - velocidadEntrada
+ * - velocidadSalida
+ *
+ * @Ejemplo
+ * ```html
+ * <div class="relato" data-color1="#19DB69" data-color2="#FFF" data-color3="#000" data-velocidadEntrada="500", data-velocidadSalida="3000">
+ *  <p>Este texto va a crecer apenas este visible el contenedor</p>
+ *  <p>Este párrafo también</p>
+ * </div>
+ *
+ * ```
+ */
 export default () => {
   const relatos = document.querySelectorAll<HTMLDivElement>('.relato');
 
   if (!relatos.length) return;
 
   relatos.forEach((seccion) => {
+    const velocidadEntrada = +seccion.dataset.velocidad || 2000;
+    const velocidadSalida = +seccion.dataset.velocidad || 2500;
+
     const parrafos = seccion.querySelectorAll<HTMLParagraphElement>('p');
 
     parrafos.forEach((parrafo) => {
@@ -36,30 +61,57 @@ export default () => {
       });
     });
 
-    duranteInterseccion(
-      seccion,
-      (elemento) => {
-        if (elemento.intersectionRatio >= 0 && elemento.intersectionRatio < 0.5) {
-          esconder(parrafos);
-          enVista = false;
-        } else {
-          mostrar(parrafos);
-          enVista = true;
-        }
+    const accion = (elemento) => {
+      const colores = [
+        [colorDesde[0], colorHacia[0]],
+        [colorDesde[1], colorHacia[1]],
+        [colorDesde[2], colorHacia[2]],
+      ];
+      const { color1, color2, color3 } = elemento.target.dataset;
+      const colorA = definirColor(color1);
+      const colorB = definirColor(color2);
+      const colorC = definirColor(color3);
+      console.log(colorA, colorB, colorC);
+      colores[0][1].color = colorA ? colorA : colorHacia[0].color;
+      colores[1][1].color = colorB ? colorB : colorHacia[1].color;
+      colores[2][1].color = colorC ? colorC : colorHacia[2].color;
+      if (elemento.intersectionRatio >= 0 && elemento.intersectionRatio < 0.5) {
+        esconder(parrafos);
+        enVista = false;
+      } else {
+        mostrar(parrafos);
+        enVista = true;
+      }
 
-        if (enVista) {
-          animarGradiente(2000, true);
-        } else {
-          setTimeout(() => {
-            if (!enVista) {
-              animarGradiente(2500, false);
-            }
-          }, 1000);
-        }
-      },
-      false,
-      { threshold: [0, 0.5, 1] }
-    );
+      if (enVista) {
+        document.documentElement.style.setProperty(
+          '--fondo1',
+          `rgb(${colores[0][1].color.r}, ${colores[0][1].color.g}, ${colores[0][1].color.b})`
+        );
+
+        document.documentElement.style.setProperty(
+          '--fondo2',
+          `rgb(${colores[1][1].color.r}, ${colores[1][1].color.g}, ${colores[1][1].color.b})`
+        );
+
+        document.documentElement.style.setProperty(
+          '--fondo3',
+          `rgb(${colores[2][1].color.r}, ${colores[2][1].color.g}, ${colores[2][1].color.b})`
+        );
+        // animarGradiente(colores, velocidadEntrada, true);
+      } else {
+        setTimeout(() => {
+          if (!enVista) {
+            document.documentElement.style.setProperty('--fondo1', null);
+            document.documentElement.style.setProperty('--fondo2', null);
+            document.documentElement.style.setProperty('--fondo3', null);
+            // animarGradiente(colores, velocidadSalida, false);
+          }
+        }, 1000);
+      }
+    };
+
+    duranteInterseccion(seccion, accion, false, { threshold: [0, 0.5, 1] });
   });
 };
 
@@ -81,13 +133,20 @@ function esconder(parrafos: NodeListOf<HTMLParagraphElement>) {
   });
 }
 
-export type ColorGradiante = {
-  porcentaje: number;
-  color: { r: number; g: number; b: number };
-};
+function definirColor(codigo: string) {
+  if (codigo && codigo.includes('#')) {
+    const color = hexARGB(codigo);
 
-function animarGradiente(duracion = 1000, adelante = true) {
-  if (reloj || (adelante && prendido)) return;
+    if (color) {
+      return { r: color[0], g: color[1], b: color[2] };
+    }
+  }
+
+  return;
+}
+
+function animarGradiente(colores: ColorGradiante[][], duracion = 1000, adelante = true) {
+  if (reloj) return;
   const segundos = duracion / 1000;
   const fotogramas = segundos * 60;
 
@@ -134,7 +193,6 @@ function animarGradiente(duracion = 1000, adelante = true) {
     if (porcentajeActual === 100 || porcentajeActual === 0) {
       clearInterval(reloj);
       reloj = null;
-      prendido = porcentajeActual === 100;
     }
   }, 16.667); // 60 cuadros por segundo
 }
