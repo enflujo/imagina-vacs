1de87c66516d4afe399ed37eb3efd10ba77aa7fc
anattolia <anattolia@gmail.com>
2023-08-22 13:57:11 -0500

    en progreso gráfica torta

---
+++ src/datos/p3ViolenciaPorGenero.ts
@@ -0,0 +1,17 @@
+const datos = {
+  infoIndicador: 'Violencia contra Niños, niñas y adolescentes. Colombia, año 2020.',
+  info: 'Fuente: FORENSIS 2020',
+  total: 5069,
+  genero: [
+    {
+      nombre: 'Niños y adolescentes hombres',
+      numero: 2392,
+    },
+    {
+      nombre: 'Niñas y adolescentes mujeres',
+      numero: 2677,
+    },
+  ],
+};
+
+export default datos;
